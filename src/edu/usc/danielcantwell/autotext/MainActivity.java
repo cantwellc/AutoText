de8928b90ff2140a549d6e8d384b73558f204c7b
dcantwell <dcantwel@usc.edu>
2013-06-25 22:30:59 -0700

    App Works.. Intended Functionality Does Not
    Only works once.

--- src/edu/usc/danielcantwell/autotext/MainActivity.java
+++ src/edu/usc/danielcantwell/autotext/MainActivity.java
@@ -4,6 +4,8 @@ import java.util.Calendar;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
@@ -40,11 +42,30 @@ public class MainActivity extends Activity {
 
 		btnSendSMS.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
+				
+				selectedHour = timePicker.getCurrentHour();
+				selectedMinute = timePicker.getCurrentMinute();
+				
+				long selectedTime = (selectedHour * 3600000) + (selectedMinute * 60000);
+				
+				Calendar c = Calendar.getInstance();
+				c.setTimeInMillis(System.currentTimeMillis());
+				c.add(Calendar.SECOND, 20);
+				
 				phoneNo = txtPhoneNo.getText().toString();
 				message = txtMessage.getText().toString();
+				
 				if (phoneNo.length() > 0 && message.length() > 0) {
-					Intent myIntent = new Intent(MainActivity.this, BackgroundActivity.class);
-					MainActivity.this.startActivity(myIntent);
+					
+					Intent myIntent = new Intent(MainActivity.this, SendMessageActivity.class);
+					PendingIntent pi = PendingIntent.getService(MainActivity.this, 0, myIntent, 0);
+					
+					AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
+					
+					alarmManager.set(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), pi);
+					
+					Toast.makeText(MainActivity.this, "Alarm Set", Toast.LENGTH_LONG).show();
+					
 				} else {
 					Toast.makeText(getBaseContext(),
 							"Please enter both phone number and message",
--- src/edu/usc/danielcantwell/autotext/SendMessageActivity.java
+++ src/edu/usc/danielcantwell/autotext/SendMessageActivity.java
@@ -5,23 +5,26 @@ package edu.usc.danielcantwell.autotext;
 
 import android.app.Activity;
 import android.app.PendingIntent;
+import android.app.Service;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.telephony.SmsManager;
+import android.widget.Toast;
 import edu.usc.danielcantwell.autotext.MainActivity;
 
 /**
  * @author Daniel
  *
  */
-public class SendMessageActivity extends Activity {
+public class SendMessageActivity extends Service {
 	
 	String phoneNo = MainActivity.phoneNo;
 	String message = MainActivity.message;
 	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+	public void onCreate() {
+		Toast.makeText(this, "Send Message Activity", Toast.LENGTH_SHORT).show();
 		
 		sendSMS(phoneNo, message);
 	}
@@ -32,6 +35,41 @@ public class SendMessageActivity extends Activity {
 				new Intent(this, SendMessageActivity.class), 0);
 		SmsManager sms = SmsManager.getDefault();
 		sms.sendTextMessage(phoneNo, null, message, pi, null);
+		
+		Toast.makeText(this, "Message Sent", Toast.LENGTH_LONG).show();
 	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		Toast.makeText(this, "MyAlarmService.onBind()", Toast.LENGTH_LONG).show();
+		return null;
+	}
+	
+	@Override
+
+	public void onDestroy() {
+
+	// TODO Auto-generated method stub
+
+	super.onDestroy();
+
+	Toast.makeText(this, "Send Message Activity onDestroy()", Toast.LENGTH_LONG).show();
+
+	}
+
+
+	@Override
+
+	public boolean onUnbind(Intent intent) {
+
+	// TODO Auto-generated method stub
+
+	Toast.makeText(this, "Send Message Activity onUnbind()", Toast.LENGTH_LONG).show();
+
+	return super.onUnbind(intent);
+
+	}
+
 
 }
