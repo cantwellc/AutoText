a0223e27ade5226ec52b42fde22e51e5698a0095
dcantwell <dcantwel@usc.edu>
2013-06-27 21:38:12 -0700

    Added Comments
    Things to Add:
    * Choose contact *
    * Send text even if app is closed *
    * Cancel text *
    * Better interface *

--- src/edu/usc/danielcantwell/autotext/MainActivity.java
+++ src/edu/usc/danielcantwell/autotext/MainActivity.java
@@ -1,7 +1,11 @@
+/**
+ * MainActivity Class
+ * extends Activity
+ * Called when app launches
+ */
 package edu.usc.danielcantwell.autotext;
 
 import java.util.Calendar;
-
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.AlarmManager;
@@ -13,6 +17,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TimePicker;
 import android.widget.Toast;
+
+/**
+ * @author Daniel
+ * 27 June 2013
+ */
+
 
 public class MainActivity extends Activity {
 
@@ -35,30 +45,36 @@ public class MainActivity extends Activity {
 		btnSendSMS = (Button) findViewById(R.id.btnSendSMS);
 		timePicker = (TimePicker) findViewById(R.id.timePicker);
 
+		// Button Click
 		btnSendSMS.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				
+				// Hour (24hr format) and Minute selected from TimePicker
 				selectedHour = timePicker.getCurrentHour();
 				selectedMinute = timePicker.getCurrentMinute();
 				
+				// Calendar to pass Selected Time to AlarmManager
 				Calendar time = Calendar.getInstance();
 				time.set(Calendar.HOUR_OF_DAY, selectedHour);
 				time.set(Calendar.MINUTE, selectedMinute);
 				time.set(Calendar.SECOND, 0);
 				
+				// Setting the Phone Number and Message
 				txtPhoneNo = (EditText) findViewById(R.id.txtPhoneNo);
 				txtMessage = (EditText) findViewById(R.id.txtMessage);
 				
 				phoneNo = txtPhoneNo.getText().toString();
 				message = txtMessage.getText().toString();
 				
+				// The Phone Number and Message must have content
 				if (phoneNo.length() > 0 && message.length() > 0) {
 					
+					// Creating the Pending Intent
 					Intent myIntent = new Intent(MainActivity.this, SendMessage.class);
 					PendingIntent pi = PendingIntent.getService(MainActivity.this, 0, myIntent, 0);
 					
+					// Creating the AlarmManager
 					AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
-					
 					alarmManager.set(AlarmManager.RTC_WAKEUP, time.getTimeInMillis(), pi);
 					
 					Toast.makeText(MainActivity.this, "Alarm Set", Toast.LENGTH_LONG).show();
--- src/edu/usc/danielcantwell/autotext/SendMessage.java
+++ src/edu/usc/danielcantwell/autotext/SendMessage.java
@@ -1,13 +1,14 @@
 /**
+ * SendMessage Class
+ * extends Service
  * 
+ * called by MainActivity
  */
 package edu.usc.danielcantwell.autotext;
 
-import android.app.Activity;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
-import android.os.Bundle;
 import android.os.IBinder;
 import android.telephony.SmsManager;
 import android.widget.Toast;
@@ -15,32 +16,37 @@ import edu.usc.danielcantwell.autotext.MainActivity;
 
 /**
  * @author Daniel
- * 
+ * 27 June 2013
  */
+
 public class SendMessage extends Service {
 
+	// Get the Phone Number and Message from the main activity
 	String phoneNo = MainActivity.phoneNo;
 	String message = MainActivity.message;
 
-	@Override
+	@Override // When this Service is first called
 	public void onCreate() {
-		Toast.makeText(this, "Send Message Activity", Toast.LENGTH_SHORT).show();
+		//Toast.makeText(this, "Send Message Activity", Toast.LENGTH_SHORT).show();
 	}
 
-	@Override
+	@Override // Each time this Service is called
 	public int onStartCommand(Intent intent, int flags, int startId) {
-		// TODO Auto-generated method stub
 
-		Toast.makeText(this, "OnStart", Toast.LENGTH_SHORT).show();
+		//Toast.makeText(this, "OnStart", Toast.LENGTH_SHORT).show();
 
+		// Send the message
 		sendSMS(phoneNo, message);
 
+		// Stop Service after the message is sent
 		stopSelf();
 		
 		return super.onStartCommand(intent, flags, startId);
 	}
 
 	private void sendSMS(String phoneNumber, String message) {
+		
+		// Create a PendingIntent for SmsManager
 		PendingIntent pi = PendingIntent.getActivity(this, 0,
 				new Intent(this, SendMessage.class), 0);
 		SmsManager sms = SmsManager.getDefault();
@@ -61,8 +67,7 @@ public class SendMessage extends Service {
 	public void onDestroy() {
 		// TODO Auto-generated method stub
 		super.onDestroy();
-		Toast.makeText(this, "Send Message Activity onDestroy()",
-				Toast.LENGTH_LONG).show();
+		//Toast.makeText(this, "Send Message Service Ended", Toast.LENGTH_LONG).show();
 	}
 
 	@Override
